4a582232eb18aab406d5f91ecdb905a9d3b443a0
DAVID CRUZ <david.cruz@actionsys.com.br>
2020-05-10 20:45:32 -0300

    alter project to docusaurus init

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,18 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  
-  title: 'ActionSys Docs', // Title for your website.
-  tagline: '"Documentação de guias da ActionSys, feshow!',
-  url: 'https://daviddossantos.github.io', // Your website URL
-  baseUrl: '/docusaurus-tutorial', // Base URL for your project */
+  title: 'Test Site', // Title for your website.
+  tagline: 'A website for testing',
+  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docusaurus-tutorial',
-  organizationName: 'daviddossantos',
+  projectName: 'test-site',
+  organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -55,8 +54,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#E33237',
-    secondaryColor: '#1f4619',
+    primaryColor: '#4b3392',
+    secondaryColor: '#342366',
   },
 
   /* Custom fonts for website */
