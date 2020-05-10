a110f17ccf15b2887331da2cf4ab92b9b3933e3a
DAVID CRUZ <david.cruz@actionsys.com.br>
2020-05-10 20:55:39 -0300

    fix:corrected error the build

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,8 +21,8 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
+  title: 'ActionSys Docs', // Title for your website.
+  tagline: 'Documentação de guias da ActionSys, feshow!',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
